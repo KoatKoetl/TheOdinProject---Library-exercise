6e940927f54ba7801cfa32ff38eb1b871bee1b62
Suschevici Stanislav <stas20027@outlook.com>
2023-08-08 09:47:24 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let Library = [];
+
+function book(name, author, releaseYear, pages) {
+  this.name = name;
+  this.author = author;
+  this.releaseYear = releaseYear;
+  this.pages = pages;
+}
+
+function addBookToLibrary() {}
