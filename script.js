f2130665a2c16dff4a2a9c6120ec9fab25e29760
Suschevici Stanislav <stas20027@outlook.com>
2023-08-09 10:21:12 +0300

    Add a delete button

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
+// Bookd values
 const title = document.getElementById("bookName");
 const author = document.getElementById("bookAuthor");
 const releaseYear = document.getElementById("releaseYear");
 const pages = document.getElementById("pages");
 
+// Button to add and div to store the card with book info
 const addBook = document.getElementById("addBook");
 const booksStorage = document.getElementById("booksStorage");
 
+// Form to fill with info about book
 const bookForm = document.getElementById("bookForm");
 
+// Image cover of book
 const cover = document.getElementById("cover");
 
+// Click on button add book call all needed functions
 addBook.addEventListener("click", function (event) {
   event.preventDefault();
   addBookToLibrary();
   showBooks();
+
+  // Clear form only when all values are entered
+  if (title.value && author.value && releaseYear.value && pages.value) {
+    // Select all input elements from the form by id
+    const inputElements = document.querySelectorAll("#bookName, #bookAuthor, #releaseYear, #pages");
+
+    // Loop through all elements and set it's values to "" after clicking buttonn Add book
+    inputElements.forEach((input) => {
+      input.value = "";
+    });
+  }
 });
 
 const myLibrary = [];
@@ -25,8 +41,9 @@ function book(title, author, releaseYear, pages) {
   this.releaseYear = releaseYear;
   this.pages = pages;
 }
-
+//Funcion to add
 function addBookToLibrary() {
+  //Check the form if it's empty then show alert otherwise push the values to array
   if (!(title.value && author.value && releaseYear.value && pages.value)) {
     alert('Fill the form. \nClick "Add new book" button to add it to your library');
   } else {
@@ -35,15 +52,38 @@ function addBookToLibrary() {
 }
 
 function showBooks() {
-  if (myLibrary.length !== 0) {
+  //Check if form insn't empty
+  if (title.value && author.value && releaseYear.value && pages.value) {
+    // Take last elemet from Array
     const book = myLibrary[myLibrary.length - 1];
+
+    //Create a div that will be card with book and add a class to it
     const card = document.createElement("div");
     card.classList.add(`card`);
     card.classList.add(`${bookOrder++}`);
+
+    //Create paragraph with all info about book
     const cardInfo = document.createElement("p");
     cardInfo.textContent = `${book.title} by ${book.author} released on ${book.releaseYear} with ${book.pages} pages`;
+
     // const bookCover = document.createElement("img");
+
+    //Create remove button
+    const removeButton = document.createElement("button");
+    removeButton.textContent = "remove";
+    removeButton.setAttribute("id", "remove-button");
+    removeButton.setAttribute("onclick", "removeBook(this)");
+
+    //Append card info and remove button on card
     card.appendChild(cardInfo);
+    card.appendChild(removeButton);
+
+    // Append card to div bookStorage on page
     booksStorage.appendChild(card);
   }
+}
+
+// Function that remove card by clicking on button
+function removeBook(e) {
+  e.parentElement.remove();
 }
